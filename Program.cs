33b1b55106a0152fe4c7d0b5c68380c2daf4c0a2
Andrey Vershinsky <vershinsky90@gmail.com>
2023-06-16 14:23:22 +0300

    task23

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Insert number");
+int num = Convert.ToInt32(Console.ReadLine());
+int lol =1;
+if (num <=0)
+{Console.WriteLine("Try again");}
+else{
+    while (lol <=num)
+    {
+        int result = lol*lol*lol;
+        Console.WriteLine(result);
+        lol++;
+    }
+}
